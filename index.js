250128db5c234ef1f84cb1c998bc68c58f44ee75
marcelomsilva <marcelo96comercial@gmail.com>
2019-07-17 15:11:51 -0300

    [update] index.js

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+/*
 app.use(cors({
-    origin:['http://localhost:4200','http://127.0.0.1:4200',],
+    origin:['http://localhost:4200','http://127.0.0.1:4200','*'],
     credentials:true
 }));
+*/
+
 
 const Employee = require('./src/routes/employee')
 const Role = require('./src/routes/role')
