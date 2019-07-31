660ceb22976c05fc34a57d587b5a1600e0606963
marcelomsilva <marcelo96comercial@gmail.com>
2019-07-31 11:16:45 -0300

    [update] cors

--- index.js
+++ index.js
@@ -6,24 +6,28 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+/*
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials',false);
-    /*
+    
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,PATCH,OPTIONS");
     next();
-    */
+    
   });
+  */
+
 /*
 app.use(cors({
     origin:['*'],
     credentials:true
 }));
 */
+app.use(cors());
 
 
 const Employee = require('./src/routes/employee')
